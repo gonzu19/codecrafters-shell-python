361fd06c776d2674bea83ef4ba9e0522cfcd6fe9
Gonzalo <gmartipo@emeal.nttdata.com>
2024-05-28 09:25:08 +0200

    added functionality for external commands

--- app/main.py
+++ app/main.py
@@ -1,11 +1,4 @@
-import sys
 import os
-
-def echo(command_array:list) -> None:
-    echo = ""
-    for word in command_array[1:]:
-        echo += f"{word} "
-        print(echo.rstrip())
 
 def shell_type(evaled_command:str) -> None:
     if evaled_command in ["echo","exit","type"]:
@@ -20,23 +13,23 @@ def shell_type(evaled_command:str) -> None:
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    # Uncomment this block to pass the first stage
     while True:
-        sys.stdout.write("$ ")
-        sys.stdout.flush()
-        # Wait for user input
-        full_command = input()
+        full_command = input("$ ")
         command_array = full_command.split()
         command = command_array[0]
         if command == "exit":
             break
         elif command == "echo":
-            echo(command_array=command_array)
+            print(' '.join(command_array[1:]))
         elif command == "type":
             shell_type(evaled_command=str(command_array[1])) 
         else:
-            print(f"{command_array[0]}: command not found")
+            found = False
+            if os.path.exists(f"{command}"):
+                os.system(f"{command} {' '.join(command_array[1:])}")
+                found = True
+            if not found:
+                print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
