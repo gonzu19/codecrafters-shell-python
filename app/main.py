5c78fc9c597022197027c24b6f8e06ec7a4bace1
Gonzalo <gmartipo@emeal.nttdata.com>
2024-05-28 08:40:28 +0200

    Now we can input multiple commands and we have an exit command

--- app/main.py
+++ app/main.py
@@ -4,13 +4,16 @@ import sys
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     # Uncomment this block to pass the first stage
-    sys.stdout.write("$ ")
-    sys.stdout.flush()
-
-    # Wait for user input
-    command = input()
-    command_array = command.split()
-    print(f"{command_array[0]}: command not found")
+    while True:
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+        # Wait for user input
+        command = input()
+        command_array = command.split()
+        if command_array[0] == "exit":
+            break
+        else:
+            print(f"{command_array[0]}: command not found")
 
 
 if __name__ == "__main__":
