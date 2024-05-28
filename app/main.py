75d938ed59cc8f39797cc66a1b1218177488cb11
Gonzalo <gmartipo@emeal.nttdata.com>
2024-05-28 09:14:05 +0200

    expanded functionality for type command

--- app/main.py
+++ app/main.py
@@ -1,4 +1,22 @@
 import sys
+import os
+
+def echo(command_array:list) -> None:
+    echo = ""
+    for word in command_array[1:]:
+        echo += f"{word} "
+        print(echo.rstrip())
+
+def shell_type(evaled_command:str) -> None:
+    if evaled_command in ["echo","exit","type"]:
+        print(f"{evaled_command} is a shell builtin")
+        return
+    paths = os.getenv("PATH").split(":")
+    for path in paths:
+            if os.path.exists(f"{path}/{evaled_command}"):
+                print(f"{evaled_command} is {path}/{evaled_command}")
+                return
+    print(f"{evaled_command} not found")
 
 
 def main():
@@ -14,16 +32,9 @@ def main():
         if command == "exit":
             break
         elif command == "echo":
-            echo = ""
-            for word in command_array[1:]:
-                echo += f"{word} "
-            print(echo.rstrip())
+            echo(command_array=command_array)
         elif command == "type":
-            evaled_command = command_array[1]
-            if evaled_command in ["echo","exit","type"]:
-                print(f"{evaled_command} is a shell builtin")
-            else:
-                print(f"{evaled_command} not found")
+            shell_type(evaled_command=str(command_array[1])) 
         else:
             print(f"{command_array[0]}: command not found")
 
